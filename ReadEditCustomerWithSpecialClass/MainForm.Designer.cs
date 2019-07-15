67bc18be319e7f036a14aba39e1bc425e7d02942
Karen Payne <paynekaren@comcast.net>
2019-07-15 04:12:09 -0700

    Added screenshot, changed title of main form

--- ReadEditCustomerWithSpecialClass/MainForm.Designer.cs
+++ ReadEditCustomerWithSpecialClass/MainForm.Designer.cs
@@ -321,7 +321,7 @@
             this.Controls.Add(this.panel1);
             this.Name = "MainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Form1";
+            this.Text = "Code sample";
             this.panel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.gridView)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.bindingNavigator1)).EndInit();
